ebff8d66301229f67dbdb2ef97dc2be1a0be1500
caris0132 <nguyenductri1h1@gmail.com>
2022-02-08 14:20:54 +0700

    blog

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Script from "next/script";
+import Layout from '@/components/layout';
+;
+
+
+export default function FirstPost() {
+  return (
+    <Layout>
+
+    </Layout>
+  );
+}
